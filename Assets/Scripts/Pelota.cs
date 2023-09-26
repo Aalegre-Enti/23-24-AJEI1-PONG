56f9627159474af71122c89651e65a5d90ce4e18
alberto.alegre_OrdenadorENTI_AJEI <alberto.alegre@enti.cat>
2023-09-26 11:17:39 +0200

    Final clase

--- Assets/Scripts/Pelota.cs
+++ Assets/Scripts/Pelota.cs
@@ -6,6 +6,11 @@ public class Pelota : MonoBehaviour
 {
     public float velocidad;
     public Vector2 direccion;
+    public Puntuacion punt;
+    private void Start()
+    {
+        Spawn();
+    }
 
     void Update()
     {
@@ -22,8 +27,33 @@ public class Pelota : MonoBehaviour
         {
             direccion = new Vector2(direccion.x, -direccion.y);
         }
+        if (collision.gameObject.tag == "Porteria A")
+        {
+            Debug.Log("He entrado en la Portería A");
+            punt.puntuacionA = punt.puntuacionA + 1;
+            Spawn();
+        }
+        if (collision.gameObject.tag == "Porteria B")
+        {
+            Debug.Log("He entrado en la Portería B");
+            punt.puntuacionB = punt.puntuacionB + 1;
+            Spawn();
+        }
+    }
 
-
-        
+    void Spawn()
+    {
+        transform.position = new Vector3(0, 0);
+        float x = Random.Range(-1f, 1f);
+        if(x > 0)
+        {
+            x = 1;
+        }
+        else
+        {
+            x = -1;
+        }
+        float y = Random.Range(-1f, 1f);
+        direccion = new Vector2 (x, y);
     }
 }
